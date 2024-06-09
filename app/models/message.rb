94640830695fc662fc87a39b760c0439397dd97b
Troy Harris <2343470+troyharris@users.noreply.github.com>
2024-06-09 12:18:15 -0700

    Added message formating helper.

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,12 @@ class Message < ApplicationRecord
   enum message_type: { event: 'event', game_information: 'game_information', tavern_information: 'tavern_information', debug: 'debug' }
 
   validates :message_type, presence: true, inclusion: { in: message_types.keys }
+
+  def formatted_time
+    hour = self.time
+    period = hour >= 12 ? 'pm' : 'am'
+    formatted_hour = hour % 12
+    formatted_hour = 12 if formatted_hour.zero?
+    "#{formatted_hour}#{period}"
+  end
 end
